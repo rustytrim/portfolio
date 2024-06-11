67c653e32a2fa27c4e4814d0e80c13128c0262be
RT <rustytrim11@gmail.com>
2024-06-11 18:14:31 -0400

    feat: add constants

---
+++ src/constants/index.ts
@@ -0,0 +1,5 @@
+export const DESCRIPTION = "Developer from North America. My goal is to continue a path in learning new things and to grow."
+export const DISCORD_URL = "https://discordlookup.com/user/526649097546104844";
+export const GITHUB_URL = "https://github.com/rustytrim";
+export const LINKEDIN_URL = "https://www.linkedin.com/in/rusty-trim-158a5727a/";
+export const X_URL = "https://x.com/rtgamingwdt"
