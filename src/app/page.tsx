c1e8e42c05aca083dc6851586c0444c6d226d330
RT <rustytrim11@gmail.com>
2024-06-11 19:11:51 -0400

    fix: style fixes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,10 @@ import { DESCRIPTION } from "@/constants";
 
 export default function Home() {
   return (
-    <main className="items-center w-5/6 !pt-[15rem]">
-      <div className="flex justify-between w-2/3">
-        <div className="w-full">
-          <h1>Hello, {"I'm"} Rusty!</h1>
+    <main className="items-center w-full !pt-[10rem] sm:!pt-[15rem]">
+      <div className="grid sm:grid-cols-2 grid-rows-2 gap-x-[20rem]">
+        <div className="w-full flex flex-col h-fit">
+          <h1>Hello {"I'm"} Rusty!</h1>
           <h2 className="text-blue-400">Self Taught Developer</h2>
           <p>{DESCRIPTION.split('.').map((str, index) => {
             return <span className="block" key={index}>{str}</span>
@@ -15,7 +15,7 @@ export default function Home() {
             <button id="bg">Skills</button>
           </div>
         </div>
-        <div className="w-[30rem] h-[20rem] rounded-full bg-[rgba(0,0,0,0.1)] flex justify-center items-center">Image Here</div>
+        <div className="w-[20rem] h-[20rem] rounded-full bg-[rgba(0,0,0,0.1)] flex justify-center items-center">Image Here</div>
       </div>
     </main>
   );
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,16 +1,30 @@
+"use client";
 import Link from "next/link";
+import { useEffect, useState } from "react";
+import { GiHamburgerMenu } from "react-icons/gi";
 
 export default function Navbar() {
+
+    const [cover, toggleCover] = useState(false);
+
+    useEffect(() => {
+        window.addEventListener("scroll", () => toggleCover(window.scrollY > 100));
+        return () => window.removeEventListener("scroll", () => toggleCover(window.scrollY > 100));
+    }, []);
+
     return (
-        <header className="w-full border-b fixed flex justify-center items-center h-16">
-            {/* <div className="fixed">test</div> */}
-            <div className="w-2/3 flex justify-between items-center">
-                <Link href="/" className="text-3xl">Rusty Trim</Link>
-                <nav className="flex gap-10 text-lg items-center">
-                    <Link href={"/projects"}>About</Link>
-                    <Link href={"/projects"}>Projects</Link>
-                </nav>
-            </div>
-        </header>
+        <>
+            <header style={{ backgroundColor: cover ? "blue" : "transparent" }} className="w-full border-b fixed flex justify-center items-center h-16">
+                <div className="w-2/3 flex justify-between items-center">
+                    <Link href="/" className="sm:text-3xl">Rusty Trim</Link>
+                    <nav className="hidden sm:flex gap-10 text-lg items-center">
+                        <Link href={"/projects"}>About</Link>
+                        <Link href={"/projects"}>Projects</Link>
+                    </nav>
+                    <GiHamburgerMenu className="sm:hidden" />
+                </div>
+            </header>
+            <div className="w-full h-[3.25rem]" />
+        </>
     )
 }
