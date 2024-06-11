3e90e58a2e3e0af9e813775bd776247dde4ea792
RT <rustytrim11@gmail.com>
2024-06-11 19:12:09 -0400

    feat: add blank target to open links in new tab

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,10 +6,10 @@ import { FaXTwitter } from "react-icons/fa6";
 export default function Footer() {
     return (
         <footer className="w-full border-t flex justify-center items-center h-16 text-4xl gap-5">
-            <Link href={DISCORD_URL}><FaDiscord /></Link>
-            <Link href={GITHUB_URL}><FaGithub /></Link>
-            <Link href={LINKEDIN_URL}><FaLinkedin /></Link>
-            <Link href={X_URL}><FaXTwitter /></Link>
+            <Link target="_blank" href={DISCORD_URL}><FaDiscord /></Link>
+            <Link target="_blank" href={GITHUB_URL}><FaGithub /></Link>
+            <Link target="_blank" href={LINKEDIN_URL}><FaLinkedin /></Link>
+            <Link target="_blank" href={X_URL}><FaXTwitter /></Link>
         </footer>
     )
 }
