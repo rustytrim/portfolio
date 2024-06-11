43e6486f51c80dd3f762c4ece45105dd270e41e9
RT <rustytrim11@gmail.com>
2024-06-11 19:11:36 -0400

    feat: add note on bottom right of screen for now

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,6 +21,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <Navbar />
+        <span className="fixed bottom-5 right-5 text-yellow-500">NOTE: Site still under development!</span>
         {children}
         <Footer />
       </body>
